46c786e13484f97eeb55858548f963ac1b3f5eef
ashura <mirror.vansh@gmail.com>
2024-10-19 08:59:29 +0530

    Initial Spring Boot Config

---
+++ springbackend/statquemo/src/main/java/com/ashuraprojs/statquemo/StatquemoApplication.java
@@ -0,0 +1,13 @@
+package com.ashuraprojs.statquemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StatquemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StatquemoApplication.class, args);
+	}
+
+}
