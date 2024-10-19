28b2fd12ca2377346b4b6540e27fe9f3edace95e
ashura <mirror.vansh@gmail.com>
2024-10-19 09:00:42 +0530

    Initial Spring Boot Config part 2

---
+++ springbackend/statquemo/src/test/java/com/ashuraprojs/statquemo/StatquemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ashuraprojs.statquemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StatquemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
